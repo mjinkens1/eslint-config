2048c5efe4b05d9e68296f24e762baade869ae13
Matt <>
2020-03-14 17:25:33 -0500

    Update rule name

--- rules/itemize/index.js
+++ rules/itemize/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "@itemizecorp/react/no-useless-computed-properties": "error",
-  "@itemizecorp/react/prefer-early-return": "error",
-  "@itemizecorp/react/react-no-multiple-render-methods": "error",
-  "@itemizecorp/react/restrict-full-import": "error"
+  '@itemizecorp/react/no-useless-computed-properties': 'error',
+  '@itemizecorp/react/prefer-early-return': 'error',
+  '@itemizecorp/react/no-multiple-render-methods': 'error',
+  '@itemizecorp/react/restrict-full-import': 'error',
 };
