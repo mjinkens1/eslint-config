afc3da5277904dbb9fb4381bd3aec1aa0923e294
Matt <>
2020-03-14 17:01:52 -0500

    Add configs for tooling config package

---
+++ configs/react-native/tooling-config.js
@@ -0,0 +1,10 @@
+/*
+ *  This file is what will be written into the project by the @itemizecorp/configure-tooling package. We don't want the rule defnitions exposed in the project
+ *  so that developers will be less likely to alter the configuration.
+ */
+
+module.exports = {
+  parser: "babel-eslint",
+  plugins: ["@itemizecorp/react"],
+  extends: "plugin:@itemizecorp/react/react-native"
+};
